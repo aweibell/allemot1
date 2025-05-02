2739783851ed6eca766d2afdbb9f9bd920e7c826
Atle Weibell <atle@weibell.no>
2025-05-02 23:11:38 +0200

    Typo: Eksperiment i fleirtal

--- src/pages/Vert/SpelVert.jsx
+++ src/pages/Vert/SpelVert.jsx
@@ -24,7 +24,7 @@ export const ExperimentList = ({
 }) => {
     return (
         <Card sx={{ p: 2, mb: 2 }}>
-            <Typography variant="h6">Eksperimenter</Typography>
+            <Typography variant="h6">Eksperiment</Typography>
             <List sx={{display:'flex', flexDirection:'column', gap:'0.4rem'}}>
                 {experiments.map((exp, index) => (
                     <ListItem
